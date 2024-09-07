4a068384b51803b7fa3738a248a1a37568d0cff7
Hode Salim <hoda.s.salim@gmail.com>
2024-09-07 16:55:49 +0300

    chore: intitalize contollers for the blog building blocks
    Post Controller
    Collections Controller
    Pages Controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def projects
+  end
+
+  def blog
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "collections/show"
+  get "posts/show"
+  get "pages/home"
+  get "pages/contact"
+  get "pages/projects"
+  get "pages/blog"
+  get "pages/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
